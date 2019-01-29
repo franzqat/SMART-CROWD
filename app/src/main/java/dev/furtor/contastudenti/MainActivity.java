8e85f340661a7e86017b59419b76913e38cd3ac4
franzqat <franz.f1032@gmail.com>
2019-01-29 18:43:49 +0100

    Initial commit

---
+++ app/src/main/java/dev/furtor/contastudenti/MainActivity.java
@@ -0,0 +1,102 @@
+package dev.furtor.contastudenti;
+
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import dev.furtor.contastudenti.helpers.MqttHelper;
+
+public class MainActivity extends AppCompatActivity {
+    MqttHelper mqttHelper;
+
+    TextView dataReceived;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (checkPermission()) {
+                     dataReceived = (TextView) findViewById(R.id.dataReceived);
+
+                startMqtt();
+        } else {
+
+            Toast.makeText(getApplicationContext(), "Impossibile inviare SMS", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    private boolean checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.WAKE_LOCK},111);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 111:
+            {
+                if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+
+                    }
+                    catch(SecurityException e){
+                        Toast.makeText(getApplicationContext(),"Impossibile",Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(),"Impossibile",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void startMqtt(){
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+
+
+}
