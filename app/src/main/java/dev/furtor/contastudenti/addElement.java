c50d49196d94d9979cbf8a7c37d7fb7702208192
franzqat <franz.f1032@gmail.com>
2019-02-01 11:42:29 +0100

    add element activity vuota

--- app/src/main/java/dev/furtor/contastudenti/addElement.java
+++ app/src/main/java/dev/furtor/contastudenti/addElement.java
@@ -1,18 +1,25 @@
 package dev.furtor.contastudenti;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class addElement extends AppCompatActivity {
+    SharedPreferences sharedPref;
+    TextView testosalvato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_element);
+
+        testosalvato = findViewById(R.id.testosalvato); //label
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,6 +29,11 @@ public class addElement extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+        /*        SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("savedPreference", testosalvato.getText().toString()); //savedPreference è la chiave
+                editor.commit();
+        */
+
             }
         });
     }
