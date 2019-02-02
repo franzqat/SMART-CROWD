ae8dc9a65543d76cc6d4acbfd7506c274d26a8df
franzqat <franz.f1032@gmail.com>
2019-02-02 21:09:13 +0100

    Update ContaPresenze.py
    documentazione py

--- Core - Raspberry/ContaPresenze.py
+++ Core - Raspberry/ContaPresenze.py
@@ -11,42 +11,42 @@ topic = "unict/didattica/aulastudio"
 qos = 1
 username = "npjfazcj"
 password = "H_BXhBstyAU4"
-
+#secondi di attesa per l'output di arp scan
 sleeptime_before_result = 5 #secondi
-# Sleep once when this script is called to give the Pi enough time
-# to connect to the network
+wait_cycle_time = 10 #tempo di attesa prima di ricominciare il ciclo
+
+# Nel caso in cui si automatizza il processo all'avvio, si aspettano 60 secondi per consentire al raspberry di collegarsi correttamente alla rete.
 #sleep(60)
-############
+
+##########
 #Functions
-############
+##########
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
-    ########################################
+
 def calcola_result():
-    # Assign the output of the arp command executed
+    # Assegna ad output il risultato del comando arp eseguito
     output = subprocess.check_output("sudo arp-scan -l", shell=True)
-    # Wait sleeptime seconds between scans
+    # aspetta sleeptime seconds
     sleep(sleeptime_before_result)
 
-    #Genera in output una lista di valori che seguono il pattern del MAC address come specificato nella variabile p tramite un regex.
-    #print(len([x.group() for x in p.finditer(output.decode("utf-8"))]))
-    #restituisce la dimensione della lista generata
+    #Genera in output una lista di valori che seguono il pattern del MAC address come specificato nella variabile p tramite un regex.  
+    #restituisce lalista generata
     result = [x.group() for x in p.finditer(output.decode("utf-8"))]
     #la conversione a set consente di eliminare i duplicati
     myset = set(result)
 	print (myset)
+	#restituisce la dimensione del set e lo converte a stringa
 	return str(len(myset))
   
-# Main thread
-
+# Main
+#pattern del regex per individuare il MAC
 p = re.compile(r'\b(?:\:?[a-f0-9]{2}){6}\b')
 
 try:
-    
-
     print("creating new instance")
     client = mqtt.Client("P1") #create new instance
     client.on_message=on_message #attach function to callback
@@ -54,18 +54,13 @@ try:
     client.username_pw_set(username, password)
     client.connect(broker_address, broker_port) #connect to broker
     client.loop_start() #start the loop
- #   print("Subscribing to topic",topic)
- #   client.subscribe(topic)
     while True:
+    	#calcola periodicamente il risultato dell'arp scan e lo pubblica
         result = calcola_result()
         print("Publishing message to topic",topic)
         client.publish(topic,result,qos)
         print("Sent! " + result)
-        time.sleep(10) # wait
-    #client.loop_stop() #stop the loop
-   # client.loop()
-
-
+        time.sleep(wait_cycle_time) # wait
 
 except KeyboardInterrupt:
     # On a keyboard interrupt signal threads to exit
